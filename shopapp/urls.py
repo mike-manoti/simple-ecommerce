e9594ff2ffc20d7ef377d934d2729f22d7d9b698
manotim <manotimike@gmail.com>
2023-10-21 19:34:56 +0300

    add to cart done v1

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -9,5 +9,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.store, name='store'),
+     path('cart/', views.cart, name='cart'),
+     path('updatecart', views.updateCart),
+     path('updatequantity', views.updateQuantity)
 ]
